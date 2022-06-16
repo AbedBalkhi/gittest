8cf1a98ed9ed4064e11fe3da542a4a69a0840f41
Abdul Balkhi <abed11balkhi@gmail.com>
2022-06-16 11:34:43 +0200

    Fist commit

--- First.java
+++ First.java
@@ -1,24 +1,32 @@
-class First{
-  static int fortyTwo(){
+class First {
+  static int fortyTwo() {
     return 42;
+    // Hi
+
   }
-  static boolean isFortyTwo(int i){
-    return i==42;
+
+  static boolean isFortyTwo(int i) {
+    return i == 42;
   }
-  static int twice(int i){
-    return i+i;
+
+  static int twice(int i) {
+    return i + i;
   }
-  static boolean isOdd(int i){
-    return i % 2 != 0; //i%2==1 || i%2==-1;
+
+  static boolean isOdd(int i) {
+    return i % 2 != 0; // i%2==1 || i%2==-1;
   }
-  static int century(int i){
-    return i/100;
+
+  static int century(int i) {
+    return i / 100;
   }
-  static int lastTwoDigits(int i){
-    return i%100;
+
+  static int lastTwoDigits(int i) {
+    return i % 100;
   }
-  static double arithmeticMean(int x, int y){
-    return (x+y)/2.0;
+
+  static double arithmeticMean(int x, int y) {
+    return (x + y) / 2.0;
   }
 
 }
